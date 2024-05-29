6b6364fb688ddaa8ab3c3ba84dc113f9519fbcab
Joseph <josephshonko99@gmail.com>
2024-05-29 09:44:52 +0300

    commiting code challenge

--- lib/debug.py
+++ lib/debug.py
@@ -1,17 +1,4 @@
-# #!/usr/bin/env python3
-# import ipdb;
 
-# from classes.many_to_many import Article
-# from classes.many_to_many import Author
-# from classes.many_to_many import Magazine
-
-# if __name__ == '__main__':
-#     print("HELLO! :) let's debug :vibing_potato:")
-    
-
-#     # don't remove this line, it's for debugging!
-
-#   ipdb.set_trace()
 
 #!/usr/bin/env python3
 import ipdb;
